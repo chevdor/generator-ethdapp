599a5378ada5e9362cf4220c758705a454179880
Will <w.kopp@kigroup.de>
2018-02-22 12:14:48 +0100

    Refactoring and fix web3 call Version bump

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,7 +1,15 @@
 function displayCurrentBlock() {
-	var elem = document.getElementById("currentBlock");
-	if (elem)
-		elem.innerHTML = window.web3.eth.blockNumber;
+    var elem = document.getElementById("currentBlock");
+    web3 = new Web3(web3.currentProvider);
+    if (elem) {
+        web3.eth.getBlockNumber(function(err, res) {
+            if (err) {
+            	console.error('getBlockNumber error', err)
+            	console.log('Did you install the parity extension? https://chrome.google.com/webstore/detail/parity-ethereum-integrati/himekenlppkgeaoeddcliojfddemadig')
+            }
+            elem.innerHTML = res;
+        })
+    }
 }
 
-setInterval(displayCurrentBlock, 1000);
+setInterval(displayCurrentBlock, 2500);
